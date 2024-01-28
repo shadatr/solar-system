8984f596d91bfdf46c62cfb04a0d55c9569b4bd1
shdab <130516923+shadatr@users.noreply.github.com>
2024-01-28 22:00:12 +0300

    first commit

---
+++ server/routes/authRoutes.js
@@ -0,0 +1,27 @@
+const passport = require('passport');
+
+module.exports = app => {
+  app.get(
+    '/api/auth/google',
+    passport.authenticate('google', {
+      scope: ['profile', 'email']
+    })
+  );
+
+  app.get(
+    '/api/auth/google/callback',
+    passport.authenticate('google'),
+    (req, res) => {
+      res.redirect('/');
+    }
+  );
+  app.get('/api/logout', (req, res) => {
+    req.logout();
+    res.redirect('/')
+  });
+
+  app.get('/api/current_user', (req, res) => {
+    console.log(req.user)
+    res.send(req.user);
+  });
+};
